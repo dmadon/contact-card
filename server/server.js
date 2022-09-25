bb723e3b70d50ca3b0362d5489239c45b39aa8d9
dmadon <deanna.madon@gmail.com>
2022-09-24 23:07:44 -0500

    implemented npm concurrently

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('../client/'));
+
+require('./routes/htmlroutes.js')(app);
+
+app.listen(PORT,function(){
+    console.log(`🌐 Now listening on port: ${PORT}`);
+})
